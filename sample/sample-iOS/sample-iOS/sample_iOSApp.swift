eb28f1c4dca2da02efe8199d0c7b1bcf7cd9dcd7
Dinakar Maurya <dinkar1708@gmail.com>
2026-09-07 15:47:22 +0900

    iOS sample project

---
+++ sample/sample-iOS/sample-iOS/sample_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_iOSApp.swift
+//  sample-iOS
+//
+//  Created by Dinakar Maurya on 2026/09/07.
+//
+
+import SwiftUI
+
+@main
+struct sample_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
